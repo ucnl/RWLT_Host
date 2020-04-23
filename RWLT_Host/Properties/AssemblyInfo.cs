6fe1a4f2d234c3d3dc1bbdb81698699ffbc9357c
Aleksandr <aleksandr.v.dikarev@gmail.com>
2020-04-23 16:40:01 +0400

    Create AssemblyInfo.cs

---
+++ RWLT_Host/Properties/AssemblyInfo.cs
@@ -0,0 +1,45 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RWLT Host")]
+[assembly: AssemblyDescription("RWLT LBL underwater positioning/tracking system host application.\n\n" +
+                               "More information about RWLT LBL\n" +
+                               "English: https://github.com/ucnl/Docs/tree/master/EN/RWLT\n" +
+                               "Russian: https://github.com/ucnl/Docs/tree/master/RU/RWLT\n\n" +
+                               "Support: mailto:support@unavlab.com\n\n" +
+                               "Source code:\n" +
+                               "https://github.com/ucnl/RWLT_Host\n")]
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Underwater communication & navigation laboratory, LLC")]
+[assembly: AssemblyProduct("RWLT Host")]
+[assembly: AssemblyCopyright("Copyright © Underwater communication & navigation laboratory, 2019-2020")]
+[assembly: AssemblyTrademark("UC&NL")]
+[assembly: AssemblyCulture("")]
+
+
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3e7f2e13-728f-4351-a016-4f10fac6f0eb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
